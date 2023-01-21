49e5649d48381170da8e595941239a4aa7ff642f
deltxprt <vincent.genest@protonmail.com>
2023-01-21 11:36:32 -0500

    pre-workout coding

--- modules/lxc.tf
+++ modules/lxc.tf
@@ -83,6 +83,9 @@ resource "azurerm_key_vault_secret" "lxcpassword" {
   name         = local.lxc_resource.hostname
   value        = local.lxc_resource.password
   key_vault_id = "/subscriptions/433a5766-0b1a-475e-aa9b-9556b6dab416/resourceGroups/Lab/providers/Microsoft.KeyVault/vaults/map-Vault-lab"
+  lifecycle {
+    ignore_changes = [password]
+  }
 }
 
 #output "lxc_resource" {
