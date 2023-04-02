dc9acba7a1a8c4d2c8e2c4f1d67a062df168c229
deltxprt <vincent.genest@protonmail.com>
2023-04-02 17:58:29 -0400

    fix #1

--- modules/lxc/variables.tf
+++ modules/lxc/variables.tf
@@ -1,31 +1,22 @@
 variable "hostname" {
-  type = string
 }
 variable "description" {
-  type = string
 }
 variable "template" {
-  type = string
 }
 variable "unprivileged" {
-  type    = bool
   default = true
 }
 variable "size" {
-  type    = string
   default = "small"
 }
 variable "onboot" {
-  type    = bool
   default = true
 }
 variable "start" {
-  type    = bool
   default = true
 }
 variable "ssh_public_keys" {
-  type = string
 }
 variable "ip_address" {
-  type = string
 }
--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -1,19 +1,14 @@
 variable "hostname" {
-  type = string
 }
 variable "description" {
-  type = string
 }
 variable "os" {
-  type = string
 }
 variable "size" {
-  type    = string
   default = "small"
 }
 variable "ip_address" {
-  type = string
 }
 variable "tags" {
-  type = list(string)
+  default = []
 }
