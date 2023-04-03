5abc5eabe38abbd20899c9c1b31fea2775dd7fad
deltxprt <vincent.genest@protonmail.com>
2023-04-03 18:25:01 -0400

    key must be fully qualified

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ data "vault_generic_secret" "dns-key" {
 provider "dns" {
   update {
     server        = "10.0.0.100"
-    key_name      = "terraform"
+    key_name      = "terraform."
     key_algorithm = "hmac-sha256"
     key_secret    = data.vault_generic_secret.dns-key.data["tsig"]
   }
